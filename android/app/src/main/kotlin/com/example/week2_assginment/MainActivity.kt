cd94b20f3ef7efb91b4f1dcde1e07da680573d51
“2020075805” <2020075805@hanyang.ac.kr>
2023-03-20 18:46:29 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/week2_assginment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.week2_assginment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:week2_assginment/screens/main_screen.dart';
+
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Chatting App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginSignUpScreen()
+    );
+  }
+}
